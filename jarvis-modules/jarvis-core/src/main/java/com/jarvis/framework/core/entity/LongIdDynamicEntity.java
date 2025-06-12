042b074d11d5218df7faa058bf727c8133b5b31d
Wang Doug <wangtao@boyescm.com>
2025-06-12 14:11:37 +0800

    style:代码整理

--- jarvis-modules/jarvis-core/src/main/java/com/jarvis/framework/core/entity/LongIdDynamicEntity.java
+++ jarvis-modules/jarvis-core/src/main/java/com/jarvis/framework/core/entity/LongIdDynamicEntity.java
@@ -81,7 +81,7 @@ public class LongIdDynamicEntity extends HashMap<String, Object> implements Base
     }
 
     protected Long processId(Object value) {
-        Long id = null;
+        long id;
         final Class<?> clazz = value.getClass();
         if (Number.class.isAssignableFrom(clazz)) {
             id = ((Number) value).longValue();
